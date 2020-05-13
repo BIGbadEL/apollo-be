f965d00c04cf53bdbc33624196aa637c9c217588
BIGbadEL <greg090912@gmail.com>
2020-05-13 12:13:08 +0200

    post for polls

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { questionSchema, Question } = require('./model/question');
-const { PollSchema, Poll } = require('./model/poll');
-const { coreSchema, Core } = require('./model/core');
-const { settingsSchema, Settings } = require('./model/settings');
+const polls = require('./routes/polls');
 
 mongoose
     .connect('mongodb+srv://apollo-io:io_Projekt@io-projekt-4qiaf.mongodb.net/test?retryWrites=true&w=majority', {
@@ -20,34 +17,11 @@ mongoose
 
             app.use(bodyParser.urlencoded({ extended: true }));
             app.use(express.json());
+            app.use('/api/polls', polls);
             app.get('/' ,(req, res) => {
                 res.send('Hello World!');
             });
             app.post('/', async (req, res) => {
-                const q = [];
-                req.body.poll.questions.forEach(element => {
-                    q.push(new Question ({
-                        id: element.id,
-                        value: element.value,
-                        options: element.options,
-                        type: element.type
-                    }));
-                });
-                const poll = new Poll({
-                    core: new Core({
-                        pinUser: req.body.init.pinUser,
-                        pinCreator: req.body.init.pinCreator,
-                        url: req.body.init.url
-                    }),
-                    questions: q,
-                    settings: new Settings({
-                        requireSignature: req.body.poll.settings.requireSignature,
-                        resultsAccess: req.body.poll.settings.resultsAccess,
-                        expire: req.body.poll.settings.expire
-                    })
-                });
-                console.log(poll);
-                await poll.save();
                 res.send("OK");
             });
             app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
--- routes/polls.js
+++ routes/polls.js
@@ -1,15 +1,37 @@
 const express = require('express');
-const { Pool, PoolSchema } = require('../model/poll');
-const { Core, coreSchema } = require('../model/core');
-const { Question, questionSchema } = require('../model/question');
+const { questionSchema, Question } = require('../model/question');
+const { PollSchema, Poll } = require('../model/poll');
+const { coreSchema, Core } = require('../model/core');
+const { settingsSchema, Settings } = require('../model/settings');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
-    const pool = new Pool({
-        
+router.post('/', async (req, res) => {
+    const q = [];
+    req.body.poll.questions.forEach(element => {
+        q.push(new Question ({
+            id: element.id,
+            value: element.value,
+            options: element.options,
+            type: element.type
+        }));
     });
+    const poll = new Poll({
+        core: new Core({
+            pinUser: req.body.init.pinUser,
+            pinCreator: req.body.init.pinCreator,
+            url: req.body.init.url
+        }),
+        questions: q,
+        settings: new Settings({
+            requireSignature: req.body.poll.settings.requireSignature,
+            resultsAccess: req.body.poll.settings.resultsAccess,
+            expire: req.body.poll.settings.expire
+        })
+    });
+    console.log(poll);
+    await poll.save();
+    res.send("OK");
 });
 
 router.get('/', (req, res) => {
@@ -19,14 +41,5 @@ router.get('/', (req, res) => {
 router.delete('/', (req, res) => {
 
 });
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
